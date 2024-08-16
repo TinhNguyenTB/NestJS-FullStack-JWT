f5b099d26d027864e1c73fc8811200bc2a02e72f
TinhNguyenTB <tinh271103@gmail.com>
2024-08-16 19:05:43 +0700

    setGlobalPrefix and enableVersioning

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
+
+  app.setGlobalPrefix('api', { exclude: [''] })
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: ['1']
+  })
 
   await app.listen(port);
 }
