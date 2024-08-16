e84857024f0ad11878c5466c1e8df7b72c04aceb
TinhNguyenTB <tinh271103@gmail.com>
2024-08-16 21:54:18 +0700

    API Get users

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -5,6 +5,7 @@ import { User } from './schemas/user.schema';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { generateHashPassword } from '@/helpers/utill';
+import aqp from 'api-query-params';
 
 @Injectable()
 export class UsersService {
@@ -29,7 +30,7 @@ export class UsersService {
       throw new BadRequestException(`Email '${email}' already exist. Please try another email.`)
     }
     // hash password
-    const hashPassword = await generateHashPassword(createUserDto.password)
+    const hashPassword = await generateHashPassword(password)
     const user = await this.userModel.create({
       name,
       email,
@@ -43,8 +44,28 @@ export class UsersService {
     };
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll(query: string, current: number, pageSize: number) {
+    const { filter, sort } = aqp(query);
+    if (filter.current) delete filter.current;
+    if (filter.pageSize) delete filter.pageSize;
+    if (!current) current = 1;
+    if (!pageSize) pageSize = 10;
+
+    const totalItems = (await this.userModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / pageSize);
+    const skip = (current - 1) * pageSize;
+
+    const results = await this.userModel
+      .find(filter)
+      .limit(pageSize)
+      .skip(skip)
+      .sort(sort as any)
+      .select(['-password'])
+
+    return {
+      totalPages,
+      results
+    };
   }
 
   findOne(id: number) {
