cd9b7f4829ea16b6b55f6bf56ad7af04370a5b09
TinhNguyenTB <tinh271103@gmail.com>
2024-08-16 20:19:06 +0700

    Validation

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { VersioningType } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +13,10 @@ async function bootstrap() {
     type: VersioningType.URI,
     defaultVersion: ['1']
   })
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true
+  }));
 
   await app.listen(port);
 }
--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,21 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsPhoneNumber } from "class-validator";
+
+export class CreateUserDto {
+    @IsNotEmpty()
+    name: string;
+
+    @IsEmail()
+    email: string;
+
+    @IsNotEmpty()
+    password: string;
+
+    @IsPhoneNumber()
+    phone: string;
+
+    @IsNotEmpty()
+    address: string;
+
+    @IsNotEmpty()
+    image: string;
+}
